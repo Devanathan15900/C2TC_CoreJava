5e46f48f0f1f4fea76fda8c434ff64cbde25af00
ELCOT <ELCOT@Lenovo>
2023-07-28 11:05:38 +0530

    day1

---
+++ main/src/Day1/Arithmetic.java
@@ -0,0 +1,14 @@
+package Day1;
+
+public class Arithmetic																																																																																																																																																																																																																																																																																																																																																																																															{
+
+	public static void main(String[] args)
+	{
+		
+     int a=17;
+     int b=56;
+     int c=a+b;
+     System.out.println("Addition:"+c);
+	}
+
+}
